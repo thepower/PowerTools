bb3543336b4af3f822ae1a50455883e58b5f53bf
Ivan Murashchenko <ivanmurashchenko@gmail.com>
2022-05-17 20:18:48 +0300

    fix(hub): fix build

--- packages/hub/src/account/sagas/index.ts
+++ packages/hub/src/account/sagas/index.ts
@@ -2,6 +2,6 @@ import { takeLatest } from 'redux-saga/effects';
 import { importAccountFromFile } from '../slice/accountSlice';
 import { importAccountFromFileSaga } from './accountSaga';
 
-export default function* () {
+export default function* accountSaga() {
   yield takeLatest(importAccountFromFile, importAccountFromFileSaga);
 }
