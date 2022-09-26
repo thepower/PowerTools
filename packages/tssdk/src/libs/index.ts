c8eadd9904dabc0cf19923f4f2b1e23f98b53a99
Семён Бабушкин <babushkin.semyon@gmail.com>
2022-09-26 16:33:03 +0800

    fix(tassdk): evm api exports

--- packages/tssdk/src/libs/index.ts
+++ packages/tssdk/src/libs/index.ts
@@ -2,6 +2,7 @@ export { AddressApi } from './address';
 export { CryptoApi } from './crypto';
 export { NetworkApi } from './network';
 export { PaymentsApi } from './payment';
+export { EvmApi } from './evm-api';
 // export { SmartContractWrapper } from './sc-interface';
 // export { instantiateSC, loadScLocal } from './sc-loader';
 export { TransactionsApi } from './transactions';
